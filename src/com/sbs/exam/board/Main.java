7ce2e42373738225b66dd4764e40b24e4a250bdb
SangWon7242 <gotkdqja@gmail.com>
2022-10-02 11:28:43 +0900

    검색 기능 구현

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -39,7 +39,26 @@ public class Main {
         System.out.println("-------------------");
         System.out.println("번호 / 제목");
 
-        List<Article> sortedArticles = articles;
+        // 검색시작
+        List<Article> filteredArticles = articles;
+
+        String searchKeyword = null;
+
+        if ( params.containsKey("searchKeyword") ) {
+          searchKeyword = params.get("searchKeyword");
+        }
+
+        filteredArticles = new ArrayList<>();
+
+        for ( Article article : articles ) {
+          boolean matched = article.title.contains(searchKeyword) || article.body.contains(searchKeyword);
+
+          if ( matched ) {
+            filteredArticles.add(article);
+          }
+        }
+
+        List<Article> sortedArticles = filteredArticles;
         boolean orderByIdDesc = true;
 
         if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
